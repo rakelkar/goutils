036e610117d685ec22c9549fb0abb0512f820c08
pansav <pansav@microsoft.com>
2021-06-28 16:56:57 -0700

    Changed lease duration in tests file from 10s to 20s

--- cmd/singleton/test_golden.sh
+++ cmd/singleton/test_golden.sh
@@ -1,4 +1,4 @@
 # expected to die gracefully after getting lock and executing delay
-./singleton -q 30s -r 5s -l 10s -cmd "./delay.sh" -args "primary running" 
+./singleton -q 30s -r 5s -l 20s -cmd "./delay.sh" -args "primary running" 
 
 ps -ef | grep singleton | grep -v grep
